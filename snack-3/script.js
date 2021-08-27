ce93b42c1c2252b1e952d8b6e4e648881802f0df
manfred-commits <manfredipiraino@gmail.com>
2021-08-27 12:51:11 +0200

    aggiunto liste

---
+++ snack-3/script.js
@@ -0,0 +1,5 @@
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+// lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+var listaDiNomi=["Mario","Mirco","Mimmo"];
+var listaDiCognomi=["Brisco","Brasco","Brango"];
+var listaFalsa=[];
