82e31553a795059c53a9505d6310788f8cede79b
manfred-commits <manfredipiraino@gmail.com>
2021-08-27 16:52:39 +0200

    aggiunta fix al while

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,14 +1,11 @@
 // Inserisci un Numero, se è pari stampa il numero, se è dispari stampa il numero successivo
 // 1.richiesta numero
 var valido=true;
-while(valido){
-    var inputUser= parseInt(prompt("Inserisci un numero: "));
-    // 1.2. validazione del numero
-    while(isNaN(inputUser)){
-        inputUser= parseInt(prompt("Inserisci un numero!!!! "));
-    }
-    valido=false;
+// 1.2. validazione del numero
+while(isNaN(inputUser)){
+    inputUser= parseInt(prompt("Inserisci un numero!!!! "));
 }
+
 // 2.verifica pari e disparii e print a video
 if(inputUser%2==0){
     alert("Il tuo numero è: "+inputUser+" NUMERO PARI");
