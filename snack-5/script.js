668136e92ad0893c4d02f777219913eed3547443
manfred-commits <manfredipiraino@gmail.com>
2021-08-27 16:12:33 +0200

    fine esercizio- aggiunto alcuni console log

--- snack-5/script.js
+++ snack-5/script.js
@@ -5,6 +5,9 @@
 // 1.Crea due array che hanno un numero di elementi diversi.
 var primo=["Bianco","Rosso","Verde","Giallo","Nero"];
 var secondo=["Banco","Osso","Ero"];
+console.log("La dimensione iniziale dell'Array primo é: "+primo.length);
+console.log("La dimensione iniziale dell'Array secondo é: "+secondo.length);
+
 //2. Aggiungi elementi casuali all’array che ha meno elementi,fino a quando ne avrà tanti quanti l’altro.
 while(primo.length!=secondo.length){
     secondo.push(primo[Math.floor(Math.random() * primo.length)]);
