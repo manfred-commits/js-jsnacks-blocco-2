bdd3c65511120e23dc49ba819f3588d698bd8e27
manfred-commits <manfredipiraino@gmail.com>
2021-08-27 17:22:53 +0200

    esercizio base snack 5 modificato

--- snack-5/script.js
+++ snack-5/script.js
@@ -3,14 +3,14 @@
 // fino a quando ne avrà tanti quanti l’altro.
 
 // 1.Crea due array che hanno un numero di elementi diversi.
-var primo=["Bianco","Rosso","Verde","Giallo","Nero"];
-var secondo=["Banco","Osso","Ero"];
+var primo=[1,2,3,4,5];
+var secondo=[1,2,3];
 console.log("La dimensione iniziale dell'Array primo é: "+primo.length);
 console.log("La dimensione iniziale dell'Array secondo é: "+secondo.length);
 
 //2. Aggiungi elementi casuali all’array che ha meno elementi,fino a quando ne avrà tanti quanti l’altro.
 while(primo.length!=secondo.length){
-    secondo.push(primo[Math.floor(Math.random() * primo.length)]);
+    secondo.push(Math.floor(Math.random() * 99));
 }
 console.log("Il primo Array, è di dimensioni: "+primo.length);
 console.log("Il secondo Array, è di dimensioni: "+secondo.length);
