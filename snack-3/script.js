084e2393f5f1cf0ccb10cec3ea21fdaba25f02b6
manfred-commits <manfredipiraino@gmail.com>
2021-08-27 13:14:55 +0200

    aggiunta piccola fix alla lista falsa

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,7 +3,17 @@
 var listaDiNomi=["Mario","Mirco","Mimmo"];
 var listaDiCognomi=["Brisco","Brasco","Brango"];
 var listaFalsa=[];
+var numeroCognomi=0;
+var numeroNomi=0;
+
 for(var i=0; i<3;i++){
-    listaFalsa.push(listaDiNomi[Math.floor(Math.random() * 2)]+" "+listaDiCognomi[Math.floor(Math.random() * 2)]);
+    listaFalsa.push(listaDiNomi[Math.floor(Math.random() * 3)]+" "+listaDiCognomi[Math.floor(Math.random() * 3)]);
+
+    // if(listaFalsa.includes(listaDiNomi[Math.floor(Math.random() * 3)]+" "+listaDiCognomi[Math.floor(Math.random() * 3)])){
+    //     i--;
+    // }else{
+    //     listaFalsa.push(listaDiNomi[Math.floor(Math.random() * 3)]+" "+listaDiCognomi[Math.floor(Math.random() * 3)]);
+    // }
+
 }
 console.log(listaFalsa);
