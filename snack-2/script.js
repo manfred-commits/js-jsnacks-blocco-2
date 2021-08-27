2d553b475168c17a2161d6e4d921ad7f61031b58
manfred-commits <manfredipiraino@gmail.com>
2021-08-27 12:16:31 +0200

    aggiunto primi step

---
+++ snack-2/script.js
@@ -0,0 +1,8 @@
+// Inserisci un Numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+// 1.richiesta numero
+var inputUser= parseInt(prompt("Inserisci un numero: "));
+console.log(inputUser);
+// 2.condizione pari
+if(inputUser%2==0){
+    alert("Il tuo numero è: "+inputUser+" NUMERO PARI");
+}
